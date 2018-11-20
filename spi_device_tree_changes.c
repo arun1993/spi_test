d3a0271dd1981f54b8badc6b64990dff42d87ce9
Arunkumar Ravichandran <arunkumar.1993.2050@gmail.com>
2018-11-20 12:42:39 -0800

    Update spi_device_tree_changes.c

--- spi_device_tree_changes.c
+++ spi_device_tree_changes.c
@@ -19,14 +19,57 @@ spi@78b9000 {
 /* 2. SPI - Louis */
 #if 0 
 
+djb_extra_spi4cs4_default: djb_extra_spi4cs4_default {
+        pinmux {
+            function = "gpio";
+            pins = "gpio62";
+        };
+        pinconf {
+            pins = "gpio62";
+            drive-strength = <16>;
+            bias-disable;
+            output-high;
+        };
+    };
 
+    djb_extra_spi4cs4_sleep: djb_extra_spi4cs4_sleep {
+        pinmux {
+            function = "gpio";
+            pins = "gpio62";
+        };
+        pinconf {
+            pins = "gpio62";
+            drive-strength = <2>;
+            bias-pull-down;
+        };
+    };
 
+spi@78b9000 {
+                    // SPI4, GPIO12:15
+                    label = "HS-SPI1";
+                    status = "okay";
+		    compatible = "spi, qcom"
+	            spi-mac-frequency = <51000>
+	            reg = <0>
+                    num-cs = <2>;
+                    cs-gpios = <&msmgpio  14 GPIO_ACTIVE_LOW>, //GPIO10  = SPI3_CS0
+                             //<&msmgpio 120 GPIO_ACTIVE_LOW>, //GPIO120 = SPI3_CS1
+                             //<&msmgpio  37 GPIO_ACTIVE_LOW>, //GPIO37  = SPI3_CS2
+                               <&msmgpio  62 GPIO_ACTIVE_LOW>; //GPIO69  = SPI3_CS3
+            	    pinctrl-names = "default", "sleep";
+                    pinctrl-0 = <&spi4_default>, <&djb_extra_spi4cs4_default>;
+                    pinctrl-1 = <&spi4_sleep>, <&djb_extra_spi4cs4_sleep>;
+	
+                };
 #endif
 
 
 /*3 SPI */
 #if 0
 
+Some other changes I made by seeing stuff from online 
+https://www.kernel.org/doc/Documentation/devicetree/bindings/spi/spi-bus.txt
+https://www.kernel.org/doc/Documentation/devicetree/bindings/spi/qcom%2Cspi-qup.txt
 
 
 #endif 
